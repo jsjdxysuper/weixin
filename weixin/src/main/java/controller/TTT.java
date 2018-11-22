018ac6149ef7ccd240856e4be81f214619bc52ff
dingxuyang <jsjdxysuper@126.com>
2018-11-22 22:49:09 +0800

    可以使用了，mybatis generator测试了

--- weixin/src/main/java/controller/TTT.java
+++ weixin/src/main/java/controller/TTT.java
@@ -1,15 +1,28 @@
 package controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import top.liyang024.sys.dao.TUrlMapper;
+import top.liyang024.sys.domain.TUrl;
+import top.liyang024.sys.domain.TUrlExample;
+
 @Controller
 public class TTT {
+	@Autowired
+	private TUrlMapper tUrlMapper;
 	@RequestMapping(value="/oka")
 	public ModelAndView lightInit() {
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("pages/Light");
+		TUrl tUrl = tUrlMapper.selectByPrimaryKey("advertising.html");
+		TUrlExample example = new TUrlExample();
+		example.createCriteria();
+		List<TUrl> tList = tUrlMapper.selectByExample(example);
 		return mv;
 	}
 }
---
+++ weixin/src/main/java/top/liyang024/sys/dao/TUrlMapper.java
@@ -0,0 +1,32 @@
+package top.liyang024.sys.dao;
+
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Component;
+
+import top.liyang024.sys.domain.TUrl;
+import top.liyang024.sys.domain.TUrlExample;
+@Component
+public interface TUrlMapper {
+    long countByExample(TUrlExample example);
+
+    int deleteByExample(TUrlExample example);
+
+    int deleteByPrimaryKey(String cId);
+
+    int insert(TUrl record);
+
+    int insertSelective(TUrl record);
+
+    List<TUrl> selectByExample(TUrlExample example);
+
+    TUrl selectByPrimaryKey(String cId);
+
+    int updateByExampleSelective(@Param("record") TUrl record, @Param("example") TUrlExample example);
+
+    int updateByExample(@Param("record") TUrl record, @Param("example") TUrlExample example);
+
+    int updateByPrimaryKeySelective(TUrl record);
+
+    int updateByPrimaryKey(TUrl record);
+}
